60e177cf636cc304728c07cadc85c05f0c37118f
Vyacheslav-Lapin <Vyacheslav.Lapin@gmail.com>
2020-11-12 16:33:10 +0300

    Initial commit

---
+++ src/main/java/ru/vlapin/experiments/Andrewexperiment/AndrewExperimentApplication.java
@@ -0,0 +1,13 @@
+package ru.vlapin.experiments.Andrewexperiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndrewExperimentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndrewExperimentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/vlapin/experiments/Andrewexperiment/AndrewExperimentApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vlapin.experiments.Andrewexperiment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndrewExperimentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
